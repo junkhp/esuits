21d823d74177909248a8131db2d6b14a1204088f
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-12-13 19:39:15 +0900

    設定修正

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
